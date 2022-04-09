b3fb1427f57d5920f73b1a8096e7c7ed1e8a3bcc
Lauricio De Souza <lauriciomalek@gmail.com>
2022-04-08 22:49:15 -0300

    criando meu primeiro servidor

---
+++ modulo01/aula01_base_servidor.js
@@ -0,0 +1,10 @@
+const http = require("http");
+
+http.createServer((requisicao,resposta) => {
+    resposta.writeHead(200,{
+        "Content-Type":"text/plain"
+    });
+
+    resposta.write("lauricio teste primeiro servidor");
+    resposta.end()
+}).listen(1337);
